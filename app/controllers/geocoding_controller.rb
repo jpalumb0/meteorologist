6ca2dc26452a7e6ea1b49df97df63a590bdd1c44
Joe <jpalumb0@chicagobooth.edu>
2017-07-11 19:18:19 +0000

    street to coords
    convert street address to lat & lng coordinates

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -14,12 +14,16 @@ class GeocodingController < ApplicationController
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    street_address_for_url = @street_address.gsub(/[^a-z0-9\s]/i, "+").strip
+    url="https://maps.googleapis.com/maps/api/geocode/json?address="+street_address_for_url+"&sensor=false"
+    parsed_data = JSON.parse(open(url).read)
+      @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+      @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
 
+    #@latitude = "Replace this string with your answer."
 
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    #@longitude = "Replace this string with your answer."
 
     render("geocoding/street_to_coords.html.erb")
   end
